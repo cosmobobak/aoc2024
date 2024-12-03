544983c12748f8636f386fc07ac2846e2c3af256
Cosmo Bobak <cbobak@esri.com>
2024-12-03 12:32:51 +0000

    clean up main()

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,14 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+#![allow(dead_code, unused_imports)]
 
 mod task01;
 mod task02;
 mod task03;
-// mod task04;
-// mod task05;
-// mod task06;
-// mod task07;
-// mod task08;
-// mod task09;
-// mod task10;
-// mod task11;
-// mod task12;
-// mod task13;
-// mod util;
 
-// use task01::task01;
-// use task02::task02;
+use task01::task01;
+use task02::task02;
 use task03::task03;
-// use task04::task04;
-// use task05::task05;
-// use task06::task06;
-// use task07::task07;
-// use task08::task08;
-// use task09::task09;
-// use task10::task10;
-// use task11::task11;
-// use task12::task12;
-// use task13::task13;
 
 fn main() {
-    task03();
+    task01();
 }
