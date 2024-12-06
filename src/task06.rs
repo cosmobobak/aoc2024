2057ad4a0d8386cb661b1e4382e13385253aeefd
Cosmo Bobak <cbobak@esri.com>
2024-12-06 11:33:00 +0000

    use smaller ints

--- src/task06.rs
+++ src/task06.rs
@@ -36,30 +36,33 @@ pub fn task06() -> Result<AocResult<usize, usize>> {
         line.as_bytes()
             .iter()
             .enumerate()
-            .filter_map(move |(col, val)| (*val == b'#').then_some((row, col)))
+            .filter_map(move |(col, val)| (*val == b'#').then_some((u16::try_from(row), u16::try_from(col))))
     });
 
-    let rows = task.lines().count();
-    let cols = task.lines().next().unwrap().len();
+    let rows = u16::try_from(task.lines().count())?;
+    let cols = u16::try_from(task.lines().next().unwrap().len())?;
 
     let mut row_map = ArrayBucket::<_, _, CAP>::new();
     let mut col_map = ArrayBucket::<_, _, CAP>::new();
 
     for (row, col) in obstacles {
+        let row = row?;
+        let col = col?;
         row_map.push(row, col);
         col_map.push(col, row);
     }
 
-    let guard_pos = task
+    let (guard_row, guard_col) = task
         .lines()
         .enumerate()
-        .find_map(|(r, l)| {
+        .find_map(|(row, l)| {
             l.as_bytes()
                 .iter()
                 .enumerate()
-                .find_map(|(c, v)| (*v == b'^').then_some((r, c)))
+                .find_map(|(col, v)| (*v == b'^').then_some((u16::try_from(row), u16::try_from(col))))
         })
         .context("couldn't find ^!")?;
+    let guard_pos = (guard_row?, guard_col?);
 
     let mut states = FxHashSet::default();
 
@@ -88,30 +91,28 @@ pub fn task06() -> Result<AocResult<usize, usize>> {
 }
 
 fn exec<const PART_2: bool>(
-    mut guard_pos: (usize, usize),
-    col_map: &ArrayBucket<usize, usize, CAP>,
-    row_map: &ArrayBucket<usize, usize, CAP>,
-    rows: usize,
-    cols: usize,
-    states: &mut FxHashSet<(Dir, usize, usize)>,
-    mut acc: impl FnMut((usize, usize)),
+    mut guard_pos: (u16, u16),
+    col_map: &ArrayBucket<u16, u16, CAP>,
+    row_map: &ArrayBucket<u16, u16, CAP>,
+    rows: u16,
+    cols: u16,
+    states: &mut FxHashSet<(Dir, u16, u16)>,
+    mut acc: impl FnMut((u16, u16)),
 ) -> bool {
     let mut dir = Dir::Up;
 
     loop {
         match dir {
             Up | Down => {
-                let row = guard_pos.0;
-                let col = guard_pos.1;
-                let obstacles = col_map.find(&col).unwrap_or(&[]);
+                let obstacles = col_map.find(&guard_pos.1).unwrap_or(&[]);
                 let new_row = if dir == Up {
-                    let Some(first_blocker) = obstacles.iter().copied().filter(|v| *v < row).max()
+                    let Some(first_blocker) = obstacles.iter().copied().filter(|v| *v < guard_pos.0).max()
                     else {
                         break;
                     };
                     first_blocker + 1
                 } else {
-                    let Some(first_blocker) = obstacles.iter().copied().filter(|v| *v > row).min()
+                    let Some(first_blocker) = obstacles.iter().copied().filter(|v| *v > guard_pos.0).min()
                     else {
                         break;
                     };
@@ -120,26 +121,24 @@ fn exec<const PART_2: bool>(
                 let min = guard_pos.0.min(new_row);
                 let max = guard_pos.0.max(new_row);
                 for r in min..=max {
-                    if PART_2 && !states.insert((dir, r, col)) {
+                    if PART_2 && !states.insert((dir, r, guard_pos.1)) {
                         return true; // repeat found
                     }
 
-                    acc((r, col));
+                    acc((r, guard_pos.1));
                 }
                 guard_pos.0 = new_row;
             }
             Left | Right => {
-                let row = guard_pos.0;
-                let col = guard_pos.1;
-                let obstacles = row_map.find(&row).unwrap_or(&[]);
+                let obstacles = row_map.find(&guard_pos.0).unwrap_or(&[]);
                 let new_col = if dir == Left {
-                    let Some(first_blocker) = obstacles.iter().copied().filter(|v| *v < col).max()
+                    let Some(first_blocker) = obstacles.iter().copied().filter(|v| *v < guard_pos.1).max()
                     else {
                         break;
                     };
                     first_blocker + 1
                 } else {
-                    let Some(first_blocker) = obstacles.iter().copied().filter(|v| *v > col).min()
+                    let Some(first_blocker) = obstacles.iter().copied().filter(|v| *v > guard_pos.1).min()
                     else {
                         break;
                     };
@@ -148,11 +147,11 @@ fn exec<const PART_2: bool>(
                 let min = guard_pos.1.min(new_col);
                 let max = guard_pos.1.max(new_col);
                 for c in min..=max {
-                    if PART_2 && !states.insert((dir, row, c)) {
+                    if PART_2 && !states.insert((dir, guard_pos.0, c)) {
                         return true; // repeat found
                     }
 
-                    acc((row, c));
+                    acc((guard_pos.0, c));
                 }
                 guard_pos.1 = new_col;
             }
