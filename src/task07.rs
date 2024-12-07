a5aa91cf047bcf9a5651e7ce671853811f2a274d
cosmobobak <cosmobobak@gmail.com>
2024-12-07 12:40:59 +0000

    cleanup

--- src/task07.rs
+++ src/task07.rs
@@ -1,53 +1,61 @@
 use anyhow::{bail, Context, Result};
+use arrayvec::ArrayVec;
 
 use crate::AocResult;
 
-fn seq_map(mut mask: u64, seq: &[i32]) -> i64 {
+fn seq_map<const OPCOUNT: u64>(mut mask: u64, seq: &[i16]) -> i64 {
     let mut vals = seq.iter();
-    let mut acc = i64::from(*vals.next().unwrap_or(&0));
+    let mut acc = i64::from(*vals.next().unwrap());
     for val in vals {
-        if (mask & 1) == 0 {
-            acc *= i64::from(*val);
-        } else {
-            acc += i64::from(*val);
+        match mask % OPCOUNT {
+            0 => acc *= i64::from(*val),
+            1 => acc += i64::from(*val),
+            2 => acc = (acc * (10i64.pow(val.ilog10() + 1))) + i64::from(*val),
+            _ => unreachable!(),
         }
-        mask >>= 1;
+        mask /= OPCOUNT;
     }
     acc
 }
 
-fn combinations_match(seq: &[i32], callback: impl Fn(i64) -> bool) -> bool {
-    // bitset of one less than the sequence-length because we
+fn combinations_match<const OPCOUNT: u64>(seq: &[i16], callback: impl Fn(i64) -> bool) -> bool {
+    // one less than the sequence-length, because we
     // insert operators *between* values.
-    let mask = (1u64 << (seq.len() - 1)) - 1;
+    let combinations = OPCOUNT.pow((seq.len() - 1).try_into().unwrap());
+    let mask = combinations.next_power_of_two() - 1;
     let mut n = 0u64;
-    loop {
-        if callback(seq_map(n, seq)) {
-            break true;
+    // carry-rippler trick:
+    for _ in 0..combinations {
+        if callback(seq_map::<OPCOUNT>(n, seq)) {
+            return true;
         }
-        n = (n.wrapping_sub(mask)) & mask;
-        if n == 0 {
-            break false;
-        }
+        n = n.wrapping_sub(mask) & mask;
     }
+
+    false
 }
 
-pub fn task07() -> Result<AocResult<i64, i32>> {
+pub fn task07() -> Result<AocResult<i64, i64>> {
     let task = std::hint::black_box(include_str!("../tasks/task07.txt"));
 
-    let mut makable = 0;
+    let mut makable1 = 0;
+    let mut makable2 = 0;
     for line in task.lines() {
         let (target, sequence) = line.split_once(": ").context("no delimiter!")?;
         let target = target.parse::<i64>()?;
         let sequence = sequence
             .split_whitespace()
             .map(str::parse)
-            .collect::<Result<Vec<i32>, _>>()?;
-        // println!("trying {target}: {sequence:?}");
-        if combinations_match(&sequence, |v| v == target) {
-            makable += target;
-        }
+            .collect::<Result<ArrayVec<i16, 32>, _>>()?;
+
+        let match1 = combinations_match::<2>(&sequence, |v| v == target);
+        let match2 = match1 || combinations_match::<3>(&sequence, |v| v == target);
+        makable1 += i64::from(match1) * target;
+        makable2 += i64::from(match2) * target;
     }
 
-    Ok(AocResult { a: makable, b: 0 })
+    Ok(AocResult {
+        a: makable1,
+        b: makable2,
+    })
 }
