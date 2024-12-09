6dae99eecb699a032a317cddddd484706698a6d2
Cosmo Bobak <cbobak@esri.com>
2024-12-09 12:01:18 +0000

    part 2 done.

--- src/task09.rs
+++ src/task09.rs
@@ -2,12 +2,135 @@ use anyhow::{bail, Context, Result};
 
 use crate::AocResult;
 
-pub fn task09() -> Result<AocResult<i64, i32>> {
+pub fn task09() -> Result<AocResult<i64, i64>> {
     let task = std::hint::black_box(include_str!("../tasks/task09.txt"));
 
-    let acc = part_1(task)?;
+    let a = part_1(task)?;
+    let b = part_2(task)?;
 
-    Ok(AocResult { a: acc, b: 0 })
+    Ok(AocResult { a, b })
+}
+
+struct Block {
+    val: i64,
+    start: usize,
+    len: usize,
+}
+
+// this is a horrifically inefficient design.
+#[allow(clippy::cast_possible_wrap, clippy::too_many_lines)]
+fn part_2(task: &str) -> Result<i64, anyhow::Error> {
+    const FREE: i64 = i64::MIN;
+
+    let mut blocks = vec![Block { val: FREE, start: 0, len: 0 }];
+    let mut free = false;
+    let mut start = 0;
+    let mut val = 0;
+    for &byte in task.as_bytes() {
+        let len = (byte - b'0') as usize;
+        if len == 0 {
+            free = !free;
+            continue;
+        }
+        if free {
+            blocks.push(Block {
+                val: FREE,
+                start,
+                len,
+            });
+        } else {
+            blocks.push(Block { val, start, len });
+            val += 1;
+        }
+        start += len;
+        free = !free;
+    }
+    // padding
+    blocks.push(Block { val: FREE, start: 0, len: 0 });
+
+    let mut try_move = blocks.len() - 1;
+    loop {
+        if try_move == 0 {
+            break;
+        }
+        // println!("try_move = {try_move}");
+        if blocks[try_move].val == FREE {
+            // empty block
+            try_move -= 1;
+            continue;
+        }
+        let mut insert = 0;
+        while insert < try_move {
+            if blocks[insert].val != FREE {
+                insert += 1;
+                continue;
+            }
+            if blocks[insert].len < blocks[try_move].len {
+                insert += 1;
+                continue;
+            }
+            // move the block in:
+            // 0. save the value
+            let val = blocks[try_move].val;
+            let len = blocks[try_move].len;
+            // 1. set the source as free-space (or dead space)
+            blocks[try_move].val = FREE;
+            // 2. unify any free space
+            match (blocks[try_move - 1].val, blocks[try_move + 1].val) {
+                (FREE, FREE) => {
+                    // if both free, nuke this one and the right-hand one, expanding the lhs into them.
+                    blocks[try_move - 1].len += blocks[try_move].len + blocks[try_move + 1].len;
+                    blocks.remove(try_move);
+                    blocks.remove(try_move);
+                    try_move -= 1;
+                }
+                (FREE, _) => {
+                    blocks[try_move - 1].len += blocks[try_move].len;
+                    blocks.remove(try_move);
+                    try_move -= 1;
+                }
+                (_, FREE) => {
+                    blocks[try_move].len += blocks[try_move + 1].len;
+                    blocks.remove(try_move + 1);
+                }
+                _ => {}
+            }
+            // 3. set the type of the freespace as our stuff
+            blocks[insert].val = val;
+            // 4. either inject remaining freespace or overwrite exactly:
+            if blocks[insert].len != len {
+                blocks.insert(insert + 1, Block {
+                    val: FREE,
+                    start: blocks[insert].start + len,
+                    len: blocks[insert].len - len,
+                });
+                try_move += 1;
+            }
+            // 5. set the first section of the freespace
+            blocks[insert].len = len;
+
+            break;
+        }
+        if try_move == 0 {
+            break;
+        }
+        try_move -= 1;
+    }
+
+    // accumulate
+    let mut acc = 0;
+    let mut count = 0;
+    for block in &blocks {
+        let val = block.len as i64;
+        if block.val >= 0 {
+            let lo = count * block.val;
+            let hi = (count + (val - 1)) * block.val;
+            acc += (lo + hi) * val / 2;
+        }
+        count += val;
+    }
+
+    Ok(acc)
 }
 
 #[allow(clippy::cast_possible_wrap)]
