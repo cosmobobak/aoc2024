e4ed1dccc8f5d1985433275c2965b12cb91588d1
Cosmo Bobak <cbobak@esri.com>
2024-12-12 15:59:14 +0000

    cleanup

--- src/task12.rs
+++ src/task12.rs
@@ -1,54 +1,64 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, ops::Index};
 
 use anyhow::{bail, Result};
+use bitvec::{order::Lsb0, view::AsMutBits};
 use fxhash::{FxBuildHasher, FxHashMap, FxHashSet};
 
 use crate::{bucket::HashBucket, AocResult};
 
-fn grow_region_from(co: (usize, usize), grid: &[Vec<u8>], region: &mut FxHashSet<(usize, usize)>) {
-    let rows = grid.len();
-    let cols = grid[0].len();
-    let val = grid[co.0][co.1];
-    for offset in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
-        let x = co.0.wrapping_add_signed(offset.0);
-        let y = co.1.wrapping_add_signed(offset.1);
+fn grow_region_from<T: Eq + Copy, A: Index<usize, Output = T> + ?Sized>(
+    (curr_x, curr_y): (u16, u16),
+    grid: &A,
+    rows: u16,
+    cols: u16,
+    region: &mut FxHashSet<(u16, u16)>,
+) {
+    let val = grid[usize::from(curr_x) * usize::from(cols) + usize::from(curr_y)];
+    for (ox, oy) in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
+        let x = curr_x.wrapping_add_signed(ox);
+        let y = curr_y.wrapping_add_signed(oy);
         if x >= rows || y >= cols {
             continue;
         }
-        if grid[x][y] != val {
+        if grid[usize::from(x) * usize::from(cols) + usize::from(y)] != val {
             continue;
         }
         if region.insert((x, y)) {
-            grow_region_from((x, y), grid, region);
+            grow_region_from((x, y), grid, rows, cols, region);
         }
     }
 }
 
-fn extract_regions(grid: &[Vec<u8>]) -> Vec<FxHashSet<(usize, usize)>> {
-    let rows = grid.len();
-    let cols = grid[0].len();
+fn extract_regions<T: Eq + Default + Copy, A: Index<usize, Output = T> + ?Sized>(
+    grid: &A,
+    rows: u16,
+    cols: u16,
+) -> Vec<FxHashSet<(u16, u16)>> {
     let mut regions = Vec::new();
 
     for row in 0..rows {
         for col in 0..cols {
-            if grid[row][col] == 0 {
+            if grid[usize::from(row) * usize::from(cols) + usize::from(col)] == T::default() {
                 continue;
             }
             let co = (row, col);
-            if regions.iter().any(|r: &FxHashSet<(usize, usize)>| r.contains(&co)) {
+            if regions
+                .iter()
+                .any(|r: &FxHashSet<(u16, u16)>| r.contains(&co))
+            {
                 continue;
             }
             regions.push(FxHashSet::default());
-            let r= regions.last_mut().unwrap();
+            let r = regions.last_mut().unwrap();
             r.insert(co);
-            grow_region_from(co, grid, r);
+            grow_region_from(co, grid, rows, cols, r);
         }
     }
 
     regions
 }
 
-fn score_region(region: &FxHashSet<(usize, usize)>) -> usize {
+fn score_region(region: &FxHashSet<(u16, u16)>) -> usize {
     let mut perimeter = 0;
     for coord in region {
         for offset in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
@@ -63,53 +73,54 @@ fn score_region(region: &FxHashSet<(usize, usize)>) -> usize {
     region.len() * perimeter
 }
 
-fn score_region_2(region: &FxHashSet<(usize, usize)>) -> usize {
-    let mut perimeter = FxHashSet::default();
-    let mut max_x = 0;
-    let mut max_y = 0;
+fn score_region_2(
+    region: &FxHashSet<(u16, u16)>,
+    rows: u16,
+    cols: u16,
+    mut grid: &mut [u8],
+) -> usize {
+    let grid = grid.as_mut_bits::<Lsb0>();
+
     for &coord in region {
-        for offset in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
-            let x = coord.0.wrapping_add_signed(offset.0);
-            let y = coord.1.wrapping_add_signed(offset.1);
+        for (i, (ox, oy)) in [(1, 0), (0, 1), (-1, 0), (0, -1)].into_iter().enumerate() {
+            let x = coord.0.wrapping_add_signed(ox);
+            let y = coord.1.wrapping_add_signed(oy);
             if !region.contains(&(x, y)) {
                 let cx = x.wrapping_add(1);
                 let cy = y.wrapping_add(1);
-                perimeter.insert(((cx, cy), offset));
-                max_x = max_x.max(cx);
-                max_y = max_y.max(cy);
+                let dir_idx = i * (usize::from(rows) * usize::from(cols));
+                let row_idx = usize::from(cx) * usize::from(cols);
+                let col_idx = usize::from(cy);
+                // potential bug from aligned edges if a shape
+                // spans the entire grid, but who cares.
+                grid.set(dir_idx + row_idx + col_idx, true);
             }
         }
     }
 
-    // construct a grid:
-    let mut grid = vec![vec![vec![0u8; max_y + 1]; max_x + 1]; 4];
-    for &((x, y), offset) in &perimeter {
-        match offset {
-            (1, 0) => grid[0][x][y] = 1,
-            (0, 1) => grid[1][x][y] = 1,
-            (-1, 0) => grid[2][x][y] = 1,
-            (0, -1) => grid[3][x][y] = 1,
-            _ => unreachable!(),
-        };
-    }
-    let mut regions = extract_regions(&grid[0]);
-    regions.extend_from_slice(&extract_regions(&grid[1]));
-    regions.extend_from_slice(&extract_regions(&grid[2]));
-    regions.extend_from_slice(&extract_regions(&grid[3]));
+    let lines = extract_regions(grid, rows * 4, cols).len();
 
-    dbg!(dbg!(region.len()) * dbg!(regions.len()))
+    region.len() * lines
 }
 
 pub fn task12() -> Result<AocResult<usize, usize>> {
     let task = std::hint::black_box(include_str!("../tasks/task12.txt"));
 
-    let grid = task.lines().map(|l| l.as_bytes().to_vec()).collect::<Vec<_>>();
-    let regions = extract_regions(&grid);
+    let grid = task
+        .lines()
+        .map(|l| l.as_bytes().to_vec())
+        .collect::<Vec<_>>();
+    let rows = grid.len().try_into()?;
+    let cols = grid[0].len().try_into()?;
+    let grid = grid.into_iter().flatten().collect::<Vec<_>>();
+    let regions = extract_regions(grid.as_slice(), rows, cols);
     let mut a = 0;
     let mut b = 0;
+    let mut grid = vec![0u8; ((usize::from(cols) + 2) * (usize::from(rows) + 2) * 4) / 8 + 1];
     for region in &regions {
         a += score_region(region);
-        b += score_region_2(region);
+        b += score_region_2(region, rows + 2, cols + 2, &mut grid);
+        grid.fill(0);
     }
 
     Ok(AocResult { a, b })
